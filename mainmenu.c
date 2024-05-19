5384a3dc94c2a0f92962d00b59a91b01f3db66b6
ninsid711 <siddy0041@gmail.com>
2024-05-19 22:45:11 +0530

    scrapping the idea of memberships, implementing searching and csv file handling in stocks functionality

--- mainmenu.c
+++ mainmenu.c
@@ -7,9 +7,8 @@ void main()
     printf("-----------------Welcome to Fusion Bakers------------------\n");
     printf("----------Please enter your choice of operation------------\n");
     printf("1. Generate bill\n");
-    printf("2. New membership\n");
-    printf("3. Check store stock\n");
-    printf("4. Exit\n");
+    printf("2. Check store stock\n");
+    printf("3. Exit\n");
     int ch;
     scanf("%d",&ch);
     switch (ch)
@@ -19,14 +18,10 @@ void main()
         // printf("Checkout\n");
         break;
     case 2:
-        // membershp();
-        printf("Membership\n");
-        break;
-    case 3:
         // stock();
         printf("Stocks\n");
         break;
-    case 4:
+    case 3:
         exit(0);
     default:
         printf("Choice not found.\n");
